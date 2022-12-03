f4b6079cd4c2f399735aabe852664d85b4bc3222
Marco Lovazzano <87663863+marcolovazzano@users.noreply.github.com>
2022-12-03 20:08:03 +0100

    fix protocol

--- .github/scripts/readme.js
+++ .github/scripts/readme.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const http = require('http');
 
 // API url
-const url = "https://api.quotable.io/random";
+const url = "http://api.quotable.io/random";
 
 function main() {
     http.get(url, res => {
